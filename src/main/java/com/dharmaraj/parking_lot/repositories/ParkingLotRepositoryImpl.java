cde8bfda1bc3796c7aa7936c51709c8358e812db
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-02-09 12:28:08 +0530

    Added repository and exception

--- src/main/java/com/dharmaraj/parking_lot/repositories/ParkingLotRepositoryImpl.java
+++ src/main/java/com/dharmaraj/parking_lot/repositories/ParkingLotRepositoryImpl.java
@@ -1,8 +1,7 @@
 package com.dharmaraj.parking_lot.repositories;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import com.dharmaraj.parking_lot.models.Gate;
@@ -10,29 +9,34 @@ import com.dharmaraj.parking_lot.models.ParkingLot;
 
 public class ParkingLotRepositoryImpl implements ParkingLotRepository {
 
-    private static final Map<Long, ParkingLot> parkingLotsById = new HashMap<>();
-    private static final Map<Long, ParkingLot> parkingLotsByGateId = new HashMap<>();
+    private final List<ParkingLot> allParkingLots = new ArrayList<>();
 
     @Override
     public Optional<ParkingLot> getParkingLotByGateId(long gateId) {
-        return parkingLotsByGateId.containsKey(gateId) ? Optional.of(parkingLotsByGateId.get(gateId)) : Optional.empty();
+        for (ParkingLot parkingLot : allParkingLots) {
+            List<Gate> gates = parkingLot.getGates();
+            for (Gate gate : gates) {
+                if (gate.getId() == gateId) {
+                    return Optional.of(parkingLot);
+                }
+            }
+        }
+        return Optional.empty();
     }
 
     @Override
     public Optional<ParkingLot> getParkingLotById(long id) {
-        return parkingLotsById.containsKey(id) ? Optional.of(parkingLotsById.get(id)) : Optional.empty();
+        for (ParkingLot parkingLot : allParkingLots) {
+            if (parkingLot.getId() == id) {
+                return Optional.of(parkingLot);
+            }
+        }
+        return Optional.empty();
     }
 
     @Override
     public ParkingLot save(ParkingLot parkingLot) {
-        long id = parkingLot.getId();
-        parkingLotsById.put(id, parkingLot);
-        List<Gate> gates = parkingLot.getGates();
-        for(Gate gate: gates) {
-            long gateId = gate.getId();
-            parkingLotsByGateId.put(gateId, parkingLot);
-        }
+        this.allParkingLots.add(parkingLot);
         return parkingLot;
     }
-    
 }
